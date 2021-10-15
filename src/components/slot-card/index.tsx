0c3837d6e564fa377593d8d58026b4672710338a
oreoluwade <oreoluwade@gmail.com>
2021-10-15 08:11:33 +0100

    Fix unwanted page reload issue

--- src/components/slot-card/index.tsx
+++ src/components/slot-card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { SlotType } from '../../types/Slot';
 import classes from './Slot-Card.module.scss';
 
@@ -8,10 +9,12 @@ type SlotCardProps = {
 
 const SlotCard = ({ slot }: SlotCardProps): JSX.Element => {
   return (
-    <a className={classes.root} href={`/slot/${slot.id}`}>
-      <h2>Slot {slot.id}</h2>
-      <p>{slot.localisedTime}</p>
-    </a>
+    <Link href={`/slot/${slot.id}`}>
+      <a className={classes.root}>
+        <h2>Slot {slot.id}</h2>
+        <p>{slot.localisedTime}</p>
+      </a>
+    </Link>
   )
 }
 
