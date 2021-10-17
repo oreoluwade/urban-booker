f49d662a50e36f01ce0189961e584cc36a872b19
oreoluwade <oreoluwade@gmail.com>
2021-10-17 14:00:19 +0100

    Add extra tests

--- src/__tests__/index-page.test.tsx
+++ src/__tests__/index-page.test.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { render } from '../../test-utils';
+import { render, screen, waitFor } from '../../test-utils';
 import Index from '../pages/index';
+import * as services from '../services';
+
+const slots = [
+  { id: 1, localisedTime: '12:00', price: '£13.00' },
+  { id: 2, localisedTime: '14:00', price: '£100.00' },
+  { id: 3, localisedTime: '15:00', price: '£79.99' }
+];
+
+const mockGetSlots = jest.spyOn(services, 'getSlots');
+mockGetSlots.mockResolvedValue(slots);
+
+jest.mock(
+  'next/link',
+  () =>
+    ({ children }: { children: React.ReactNode }) =>
+      children
+);
 
 describe('Index', () => {
-  it('Renders the Entry point into the application without crashing', () => {
-    const { getByText } = render(<Index />);
-    const pageTitle = getByText('Welcome to our scheduling page');
-    expect(pageTitle).toBeInTheDocument();
+  it('Renders the Entry point into the application without crashing', async () => {
+    render(<Index />);
+    await waitFor(() => {
+      expect(
+        screen.getByText('Welcome to our scheduling page')
+      ).toBeInTheDocument();
+    });
   });
 });
